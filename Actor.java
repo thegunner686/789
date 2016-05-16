bb62552365cdcba6c0947619f84e4e3c9e33904a
Kzdawg <kevinzhang1199@gmail.com>
2016-05-16 08:59:16 -0700

    Update Actor.java

--- Actor.java
+++ Actor.java
@@ -41,7 +41,7 @@ public class Actor {
 	//postcondition: this actor is now at the location l.getRow(), l.getCol() in the Grid 
 	// G, and myGrid has been updated to be g, gl is updated to be l, pixel location is
 	// updated, jc is initialized and put into the GUI,
-	public void putSelfInGrid(GameWorld gw, Grid g, GridLocation l){
+	public void putSelfInGrid(Grid g, GridLocation l){
 		myGrid = g;
 		gl = l;
 		pl = null;
@@ -49,9 +49,16 @@ public class Actor {
 	}
 	
 	
-	//postcondition: this actor is removed from myGrid, and myGrid is now null, gl is nul
+	//postcondition: this actor is removed from myGrid, and myGrid is now null, gl is null
 	public void removeSelfFromGrid(){
 		myGrid = null;
 		gl = null;
+	}
+	
+	public int hashCode(){
+		int hash = damage*21 + health;
+		hash = hash*5 + gl.getRow();
+		hash = hash*7 + gl.getCol();
+		return hash;
 	}
 }
